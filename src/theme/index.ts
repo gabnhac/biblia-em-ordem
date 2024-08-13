c79b757ff6461866e81ed72d204004fca4ff088d
unknown <gabriel.lucena@b2list.com>
2024-08-13 12:58:05 -0300

    feat: criação do tema, e importação da styled components e das fonts

---
+++ src/theme/index.ts
@@ -0,0 +1,21 @@
+export default {
+    COLORS: {
+        FRENCH_GRAY: '#B9C1C7',
+        PRUSSIAN_BLUE: '#102A41',
+        PRINCETON_ORANGE: '#F7900B',
+        PLATINUM: '#E9EAEA'
+    },
+
+    FONT_FAMILY: {
+        REGULAR: 'Roboto_400Regular',
+        BOLD: 'Roboto_700Bold'
+    },
+
+    FONT_SIZE: {
+        SM: 14,
+        MD: 16,
+        LG: 18,
+        XL: 24
+    }
+
+}
